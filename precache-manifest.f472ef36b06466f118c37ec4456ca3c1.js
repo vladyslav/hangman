edd47ab97e8b2b940bab6970237e14b612d43195
Vladyslav Vasylenko <vladyslav.vasylenko@dev.pro>
2020-08-12 21:53:05 +0300

    Updates

---
+++ precache-manifest.f472ef36b06466f118c37ec4456ca3c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899db7042e3fedbc90e2700ea56dbdd5",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "f70cc4a14587c31d0d9a",
+    "url": "/hangman/static/css/main.bab13ed4.chunk.css"
+  },
+  {
+    "revision": "973e31a4495480d1933b",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f70cc4a14587c31d0d9a",
+    "url": "/hangman/static/js/main.13adf17a.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
